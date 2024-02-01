f05c640aba44afa47ed5ff9af0f11d7a6b578186
Salah Eddine <shen.real@live.fr>
2024-02-01 03:37:36 -1200

    fixing error

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,22 +7,35 @@ import os
 
 env.hosts = ["35.153.50.198", "54.234.98.62"]
 
+
+@task
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
-        return False
+    """Fabric script that distributes an archive to web servers"""
     try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        if not os.path.exists(archive_path):
+            return False
+        with_ext = archive_path.split("/")[-1]
+        without_ext = archive_path.split("/")[-1].split(".")[0]
+        pth = "/data/web_static/releases/"
+        put(archive_path, "/tmp/")
+        run("mkdir -p " + pth + without_ext)
+        run("tar -xzf /tmp/{} -C {}{}/".format(with_ext, pth, without_ext))
+        run("rm /tmp/{}".format(with_ext))
+        run("mv {1}{0}/web_static/* {1}{0}/".format(without_ext, pth))
+        run("rm -rf {}{}/web_static".format(pth, without_ext))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {}{}/ /data/web_static/current".format(pth, without_ext))
         return True
-    except:
+    except Exception:
         return False
+
+
+@task
+def do_pack():
+    """generates a .tgz archive from web_static"""
+    datestr = datetime.now().strftime("%Y%m%d%H%M%S")
+    file_name = "versions/web_static_{}.tgz".format(datestr)
+    local("mkdir -p versions")
+    if local("tar -cvzf {} web_static/".format(file_name)).succeeded:
+        return file_name
+    return None
